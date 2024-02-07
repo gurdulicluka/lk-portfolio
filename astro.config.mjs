be95034c490764ba726788f1de07f905763f35bd
lgurdulic <luka.gurdulic@hivetech.hr>
2024-02-07 21:27:32 +0100

    starter storyblok files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,23 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 import storyblok from '@storyblok/astro';
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import { loadEnv } from 'vite'
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
+ 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/home': '/'
+  },
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
   integrations: [tailwind(), react(), storyblok({
-    accessToken: 'I4y4GYUpCCoBJrf9Bb4Fsgtt',
+    accessToken: env.STORYBLOK_TOKEN ,
     components: {
       page: 'storyblok/Page',
       feature: 'storyblok/Feature',
